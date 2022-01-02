811e61c024b349dc641e39f2264cabc632d6dacc
Ricky <Ricky@DESKTOP-OHE24P9>
2022-01-01 20:14:51 -0400

    Agregar archivos de proyecto.

---
+++ WebApiBook/Data/WebApiBookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiBook.Models;
+
+namespace WebApiBook.Data
+{
+    public class WebApiBookContext : DbContext
+    {
+        public WebApiBookContext (DbContextOptions<WebApiBookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiBook.Models.Book> Book { get; set; }
+    }
+}
---
+++ WebApiBook/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiBook.Models
+{
+    public class Book
+    {
+
+        public int id { get; set; }
+
+        public string  Title { get; set; }
+
+        public string  Author { get; set; }
+
+        public int Pages { get; set; }
+    }
+}
